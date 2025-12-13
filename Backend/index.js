af244f2b1ecde1194f4fc2de00f2e2936b0472f2
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2025-12-14 00:01:41 +0530

    connect the database

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express=require('express');
 const PORT=process.env.PORT;
+const database=require('./config/database');
 const app=express();
 
 app.use(express.json());
 
+database();
 app.get('/',(req,res)=>{
     res.json({message:'this is the home page'});
 });
